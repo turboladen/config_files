3e24d4ce8af5964c5fad696d84a1f2f18516718b
Steve Loveless <sloveless@sloveless-mbp.local>
2015-07-08 10:57:02 -0700

    Make the foreman window 3%, don't start server

--- tmux/tmux_work_env.sh
+++ tmux/tmux_work_env.sh
@@ -4,9 +4,7 @@ CURRENT_SESSION=${PWD##*/}
 
 # Start up the tmux session with specific name. First window is for editing.
 tmux new-session -s $CURRENT_SESSION -n editor -d
-
-# VIM window
-tmux send-keys -t $CURRENT_SESSION 'vim' C-m
+tmux send-keys -t $CURRENT_SESSION:1.1 "vim" C-m
 
 # RSpec pane for running tests
 # tmux split-window -v -p 20 -t $CURRENT_SESSION
@@ -15,18 +13,17 @@ tmux send-keys -t $CURRENT_SESSION 'vim' C-m
 
 # Window for running development server
 tmux new-window -n servers -t $CURRENT_SESSION
-# tmux split-window -v -p 20 -t $CURRENT_SESSION:2
-tmux split-window -v -t $CURRENT_SESSION:2
-tmux select-layout -t $CURRENT_SESSION:2 main-horizontal
+tmux split-window -v -p 97 -t $CURRENT_SESSION:2
+# tmux select-layout -t $CURRENT_SESSION:2 main-horizontal
 
 # Run core and dependent apps
 tmux send-keys -t $CURRENT_SESSION:2.1 'z core' C-m
 tmux send-keys -t $CURRENT_SESSION:2.1 'foreman start' C-m
 
 # Run the server
-tmux send-keys -t $CURRENT_SESSION:2.2 'bin/rails server' C-m
+# tmux send-keys -t $CURRENT_SESSION:2.2 'bin/rails server' C-m
 
-# Select the first window
+# Select the vim window and run vim
 tmux select-window -t $CURRENT_SESSION:1
 
 # Attach the tmux session
